e74dee8e5b2ae218129d9a63b50012f77db2bdcd
Connor Shorten <connorshorten300@gmail.com>
2022-09-22 12:32:39 -0400

    Update build.sh

--- cicd/build.sh
+++ cicd/build.sh
@@ -4,5 +4,6 @@ set -e
 
 local_repo=${LOCAL_REPO?Variable LOCAL_REPO is required}
 model_name=${MODEL_NAME?Variable MODEL_NAME is required}
+tokenizer_name=${TOKENIZER_NAME?Variable TOKENIZER_NAME is required}
 
-docker build --build-arg "MODEL_NAME=$model_name" -t "$local_repo" .
+docker build --build-arg "MODEL_NAME=$model_name" --build-arg "TOKENIZER_NAME=$tokenizer_name" -t "$local_repo" .
